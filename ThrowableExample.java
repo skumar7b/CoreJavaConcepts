6c720b17c3ed0a45982c6164f1e3202b68974727
skumar7b <55125632+skumar7b@users.noreply.github.com>
2019-09-11 06:24:38 -0400

    Update ThrowableExample.java

--- ThrowableExample.java
+++ ThrowableExample.java
@@ -1,46 +1,24 @@
 
 import java.io.*; 
+import java.util.properties;
 
-class ThrowableExample { 
+class ThrowableExample 
+{
+public static void main(String[] args) throws Throwable {
+  Properties pr = new Properties();
+  FileInputStream fi = new FileInputStream("clogg.properties");
+  stringToDouble("100.5");
+}
   
-    // Main Method 
-    public static void main(String[] args) 
-        throws Exception 
-    { 
-  
-        try { 
-  
-            testException1(); 
-        } 
-  
-        catch (Throwable e) { 
-  
-            // access to the stack trace 
-            StackTraceElement[] trace = e.getStackTrace(); 
-  
-            System.out.println("Thrown "+trace[0].toString()); 
-        } 
-    } 
-  
-    // method which throws Exception 
-    public static void testException1() 
-        throws Exception 
-    { 
-  
-        // create a new Exception 
-        Exception ex = new Exception(); 
-  
-        StackTraceElement[] trace = new StackTraceElement[] { 
-            new StackTraceElement("ClassNameOfExe", 
-                                  "methodNameOfExe", 
-                                  "fileNameOfExe", 
-                                  10) 
-        }; 
-  
-        // sets the stack trace elements 
-        ex.setStackTrace(trace); 
-  
-        // throw the Throwable[ 
-        throw ex; 
-    } 
-} 
+  public static void sringToDouble(String s){
+   double myd = 0.0;
+   try{
+     myd = Double.parseDouble(s);
+     System.out.println("After converting String to double");
+     
+   }catch(Throwable t){
+     System.out.println("this is the catch block");
+   }
+    System.out.println("This is the value of myd --"+myd);
+  }
+}
