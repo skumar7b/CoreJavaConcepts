23c2a22f345787973b9a25b0beb2d6025818a15f
skumar7b <55125632+skumar7b@users.noreply.github.com>
2019-09-11 05:00:20 -0400

    Update UncheckedExceptionExample.java

--- UncheckedExceptionExample.java
+++ UncheckedExceptionExample.java
@@ -1,22 +1,7 @@
-
-import java.io.FileReader;
-import java.io.IOException;
-
-public class UncheckedExceptionExample {
-	public static void main(String[] args)
-	{
-	    try
-	    {
-	        FileReader file = new FileReader("pom.xml");
-	         
-	        file = null;
-	         
-	        file.read();
-	    }
-	    catch (IOException e)
-	    {
-	        //Alternate logic
-	        e.printStackTrace();
-	    }
-	}
+class UncheckedExceptionExample {  
+   public static void main(String args[])
+   {
+	int arr[] ={1,2,3,4,5};
+	System.out.println(arr[7]);
+   }
 }
